2805615f45ed091e00c33dc44ba81c1cc4591081
elmiramousaie <150168045+elmiramousaie@users.noreply.github.com>
2023-11-11 15:31:46 +0330

    Add files via upload

---
+++ main cake.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf(" o o o o o \n o o O o o \n o o o o o ");
+    return 0;
+}
---
+++ main first letter.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf(" E E E E E\n E \n E \n E \n E E E E E \n E \n E \n E \n E E E E E");
+    return 0;
+}
